d93cd7685e86f9d1cfa30ef59a6d41f15db71dd3
Victor Prete <vprete@tuta.io>
2022-09-11 22:18:58 -0400

    feat: add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import Navbar from '../components/Navbar'
+import Card from '../components/Card'
+import data from '../data'
 
 function App() {
+  const cards = data.map(item => {
+      return (
+          <Card
+              key={item.id}
+              img={item.coverImg}
+              rating={item.stats.rating}
+              reviewCount={item.stats.reviewCount}
+              location={item.location}
+              title={item.title}
+              price={item.price}
+              openSpots={item.openSpots}
+          />
+      )
+  })
+
   return (
-    <>
-      <Navbar />
-    </>
+      <div>
+          <Navbar />
+          <section className="cards-list">
+              {cards}
+          </section>
+      </div>
   )
 }
 
